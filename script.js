0a5164bbebb78cc32bfb5fbcde04d21e9f11a659
TechDan09 <techdan09@gmail.com>
2021-12-21 14:34:45 +0200

    implmenting add and remove item to cart

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 const menuBtn = document.querySelector('.mobile_menu');
 const nav = document.querySelector('.nav_links');
 const navLinks = nav.childNodes;
+const minusBtn = document.getElementById('minus_btn');
+const plusBtn = document.getElementById('plus_btn');
+const cartInput = document.getElementById('cart_input');
+const addToCartBtn = document.querySelector('.add_to_cart_btn');
+const productPrice = 125;
+
+let totalQuantity = Number(cartInput.textContent);
+let totalPrice = totalQuantity * 125;
 
 menuBtn.addEventListener('click', () => {
   nav.classList.toggle('open');
@@ -12,4 +20,24 @@ const closeNav = () => {
 
 navLinks.forEach((link) => {
   link.addEventListener('click', closeNav);
+});
+
+minusBtn.addEventListener('click', () => {
+  // prevent negatiave cart value
+  if (totalQuantity > 0) {
+    totalQuantity--;
+    cartInput.textContent = totalQuantity;
+    totalPrice = totalQuantity * 125;
+  }
+});
+
+plusBtn.addEventListener('click', () => {
+  // prevent negatiave cart value
+  totalQuantity++;
+  cartInput.textContent = totalQuantity;
+  totalPrice = totalQuantity * 125;
+});
+
+addToCartBtn.addEventListener('click', () => {
+  console.log(`Quantity in cart: ${totalQuantity} | Total Price: ${totalPrice}`);
 });
